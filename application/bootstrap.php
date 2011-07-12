64a2b9860dcb24b4cc5a0efe17d0bbb59f46e65a
Ragsnas <ansgar.schuermann@psx-technology.com>
2011-07-12 15:23:45 +0200

    tiny steps
    added public index file.
    added htaccess
    made minor changes to bootstrap

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -3,3 +3,4 @@
 /**
  * This is where the bootstrapping will go
  */
+
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * File to call public in browser and therefore to handle all requests
+ */
+ include_once 'application/bootstrap.php';
+ 
