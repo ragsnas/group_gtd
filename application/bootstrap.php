cc1625b6489c9b6778d0e8498c20e1ce5de3192b
Ragsnas <ansgar.schuermann@psx-technology.com>
2011-07-12 16:19:10 +0200

    first commit inside eclipse i think.

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -4,6 +4,9 @@
  * This is where the bootstrapping will go
  */
 
+if(!defined('APPLICATION_ENV')) {
+	define('APPLICATION_ENV','production');
+}
 if(!defined('APPLICATION_PATH')) {
 	define('APPLICATION_PATH',realpath(dirname(__FILE__)));
 }
--- public/index.php
+++ public/index.php
@@ -3,5 +3,7 @@
 /**
  * File to call public in browser and therefore to handle all requests
  */
- include_once 'application/bootstrap.php';
  
+include_once 'application/bootstrap.php';
+
+die ('So much for now! Mostly testing git/github/EGit at the moment.');
